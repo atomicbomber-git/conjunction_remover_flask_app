8e427acf578656985cdeb003642b7792730d4065
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-09-16 16:42:30 +0700

    Created project.

---
+++ app.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/flask_app')
+
+from app.serve import app as application
---
+++ app/serve.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import os
+import nltk
+from nltk.tag import CRFTagger
+import numpy as np
+
+app = Flask(__name__)
+
+ct = CRFTagger()
+ct.set_model_file(os.path.dirname(os.path.abspath(__file__)) + '/all_indo_man_tag_corpus_model.crf.tagger')
+
+@app.route('/')
+def process():
+    # Tokenize input text
+    input_text = nltk.word_tokenize(request.args.get('input', ''))
+
+    # Tag sentence
+    result = ct.tag_sents([input_text])
+
+    # Remove unwanted elements
+    forbidden_tags = ['SC', 'IN', 'CC']
+    for index, sentence in enumerate(result):
+        result[index] = [word for word in sentence if word[1] not in forbidden_tags]
+
+    # Assemble output
+    output = ''
+    for sentence in result:
+        for word in sentence:
+            output = output + ' ' + word[0]
+
+    return jsonify({
+        'result': output.strip(),
+        'status': 'success'
+    })
+
+if __name__ == '__main__':
+    app.run()
